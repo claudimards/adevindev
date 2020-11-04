07258b7f19c607d9de20aee70f7a03dbb261a146
claudimar <claudimards@gmail.com>
2020-11-03 22:20:24 -0300

    Header finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* Menu Show */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if (toggle && navId){
+        toggle.addEventListener('click', ()=> {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/* Active and Remove Menu */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    // Active link
+    navLink.forEach(nav => nav.classList.remove('active'))
+    this.classList.add('active')
+
+    // Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach( nav => nav.addEventListener('click', linkAction))
